223b75ba4f7912dd946f790b09842f19c4ab9556
Amudhesh-KT <damudheshkt@gmail.com>
2023-06-06 12:40:20 +0530

    Check for working{completed 3 options]

--- actions/actions.py
+++ actions/actions.py
@@ -1,5 +1,5 @@
 from typing import Any, Text, Dict, List, Union
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Tracker, FormValidationAction, Action
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 from rasa_sdk.interfaces import ActionExecutionRejection
@@ -23,45 +23,45 @@ class ActionGreet(Action):
 
 #**************************************************4 usecases of application*******************************************
 
-class ActionComplaint(Action):
-    def name(self) -> Text:
-        return "action_complaint"
+# class ActionComplaint(Action):
+#     def name(self) -> Text:
+#         return "action_complaint"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_complaint")
-        return []
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         dispatcher.utter_message(response="utter_complaint")
+#         return []
 
-class ActionTrackComplaint(Action):
-    def name(self) -> Text:
-        return "action_track_complaint"
+# class ActionTrackComplaint(Action):
+#     def name(self) -> Text:
+#         return "action_track_complaint"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_track_complaint")
-        return []
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         dispatcher.utter_message(response="utter_track_complaint")
+#         return []
 
-class ActionSuggestion(Action):
-    def name(self) -> Text:
-        return "action_suggestion"
+# class ActionSuggestion(Action):
+#     def name(self) -> Text:
+#         return "action_suggestion"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_suggestion")
-        return []
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         dispatcher.utter_message(response="utter_suggestion")
+#         return []
 
-class ActionApplyService(Action):
-    def name(self) -> Text:
-        return "action_apply_service"
+# class ActionApplyService(Action):
+#     def name(self) -> Text:
+#         return "action_apply_service"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_apply_service")
-        return []
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         dispatcher.utter_message(response="utter_apply_service")
+#         return []
 
 #**************************************************4 usecases of application*******************************************
 
@@ -108,3 +108,179 @@ class ActionApplyService(Action):
 #         return []
 
 #**************************************************Raise a complaint form*******************************************
+
+#****************************************RAISE_COMPLAINT****************************************************
+class ValidateComplaintForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_raise_complaint_form"
+
+    def validate_r_username(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    
+
+ 
+        dispatcher.utter_message(text=f"Your user name is {slot_value}.")
+        return {"r_username": slot_value}
+    
+    def validate_r_email(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    
+
+ 
+        dispatcher.utter_message(text=f"Your email is {slot_value}.")
+        return {"r_email": slot_value}
+    
+
+    def validate_r_location(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    
+
+ 
+        dispatcher.utter_message(text=f"Your location is {slot_value}.")
+        return {"r_location": slot_value}
+
+    def validate_r_complaint_details(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    
+
+ 
+        dispatcher.utter_message(text=f"Your complaint details is {slot_value}.")
+        return {"r_complaint_details": slot_value}
+    
+    def validate_r_attachments(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    
+
+ 
+        dispatcher.utter_message(text=f"Your attachments is {slot_value}.")
+        return {"r_attachments": slot_value}
+#****************************************RAISE_COMPLAINT****************************************************    
+
+#****************************************TRACK COMPLAINT******************************************************   
+class ValidateTrackComplaintForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_track_complaint_form"
+    
+    def validate_t_complaint_number(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    
+
+ 
+        dispatcher.utter_message(text=f"Your complaint number is {slot_value}.")
+        return {"t_complaint_number": slot_value}
+    
+    def validate_t_comments(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    
+
+ 
+        dispatcher.utter_message(text=f"Your comment is {slot_value}.")
+        return {"t_comments": slot_value}
+#****************************************TRACK COMPLAINT******************************************************
+
+#****************************************SUGGESTIONS**********************************************************
+class ValidateSuggestionForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_suggesstion_form"
+
+    def validate_s_username(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    
+
+ 
+        dispatcher.utter_message(text=f"Your user name is {slot_value}.")
+        return {"s_username": slot_value}
+    
+    def validate_s_email(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    
+
+ 
+        dispatcher.utter_message(text=f"Your email is {slot_value}.")
+        return {"s_email": slot_value}
+    
+
+    def validate_s_location(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    
+
+ 
+        dispatcher.utter_message(text=f"Your location is {slot_value}.")
+        return {"s_location": slot_value}
+
+    def validate_s_complaint_details(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    
+
+ 
+        dispatcher.utter_message(text=f"Your complaint details is {slot_value}.")
+        return {"s_complaint_details": slot_value}
+    
+    def validate_s_attachments(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    
+
+ 
+        dispatcher.utter_message(text=f"Your attachments is {slot_value}.")
+        return {"s_attachments": slot_value}
+#****************************************SUGGESTIONS**********************************************************
